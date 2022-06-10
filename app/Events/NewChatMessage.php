47bf0ab61bb8a3b8d4494b2f40bec876213c7fe1
Kaan Serin <kaanserin@Kaans-MacBook-Pro.local>
2022-06-10 23:26:39 +0300

    Install pusher

---
+++ app/Events/NewChatMessage.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\ChatMessage;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewChatMessage implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $chatMessage;
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, ChatMessage $chatMessage)
+    {
+        $this->user = $user;
+        $this->chatMessage = $chatMessage;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('chat.' . $this->chatMessage->chat_room_id);
+    }
+
+    /**
+     * The event's broadcast name.
+     *
+     * @return string
+     */
+    public function broadcastAs()
+    {
+        return 'message';
+    }
+}
