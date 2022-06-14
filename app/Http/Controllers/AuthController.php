902846efdfe8311660ebd51907d99271ad0e9107
AlanSerin <alanserin114@gmail.com>
2022-06-14 14:06:53 +0300

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Enum\UserTypeEnum;
+use App\Models\Agent;
+use App\Models\Authority;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -45,6 +47,50 @@ class AuthController extends Controller
         ]);
     }
 
+    public function verifyPhone(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'request_id' => 'required|string',
+            'code' => 'required|string',
+            'type' => 'required|string',
+            'id' =>   'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return res($validator->errors(), 400);
+        }
+
+
+        $basic  = new \Vonage\Client\Credentials\Basic("47ac5dca", "CUcRGKKwEyqV0AlI");
+        $client = new \Vonage\Client(new \Vonage\Client\Credentials\Container($basic));
+
+        try {
+            $result = $client->verify()->check($request->request_id, $request->code);
+            var_dump($result->getResponseData());
+
+            if(compareWithEnum($request->type, UserTypeEnum::AUTHORITY)) {
+                $authority = Authority::query()->find($request->id);
+                if ($authority) {
+                    $authority->verified = 1;
+                    $authority->save();
+                }
+            }
+            else if(compareWithEnum($request->type, UserTypeEnum::AGENT)) {
+                $agent = Agent::find($request->id);
+                if($agent) {
+                    $agent->verified = 1;
+                    $agent->save();
+                }
+            }
+
+            return 'success';
+        } catch (\Exception $e) {
+            return res($e->getMessage(), 500);
+        }
+
+    }
+
     public function register(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
@@ -52,6 +98,7 @@ class AuthController extends Controller
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|confirmed',
             'type' => [new Enum(UserTypeEnum::class)],
+            'phone' => 'required|numeric',
         ]);
 
         if ($validator->fails()) {
@@ -65,7 +112,15 @@ class AuthController extends Controller
                 'email' => $request->email,
                 'password' => bcrypt($request->password),
                 'type' => $request->type,
+                'phone' => $request->phone,
+                'verified' => 0,
             ]);
+
+            $basic  = new \Vonage\Client\Credentials\Basic("47ac5dca", "CUcRGKKwEyqV0AlI");
+            $client = new \Vonage\Client(new \Vonage\Client\Credentials\Container($basic));
+
+            $request = new \Vonage\Verify\Request($request->phone, "Emergency");
+            $response = $client->verify()->start($request);
 
             $user->save();
 
@@ -95,6 +150,7 @@ class AuthController extends Controller
             return res([
                 'user' => $user,
                 'token' => $user->createToken('Personal Access Token')->plainTextToken,
+                'request_id' => $response->getRequestId()
             ]);
         } catch (\Exception $e) {
             DB::rollBack();
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
  */
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('verify', [\App\Http\Controllers\AuthController::class, 'verifyPhone']);
     Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
 });
 
