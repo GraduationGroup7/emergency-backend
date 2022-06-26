402cb084a60b644d7b7a007032da45f09d448a7e
Kaan Serin <kaanserin@Kaans-MacBook-Pro.local>
2022-06-26 17:32:45 +0300

    Fix seeder

--- app/Http/Controllers/ChatRoomController.php
+++ app/Http/Controllers/ChatRoomController.php
@@ -94,7 +94,7 @@ class ChatRoomController extends Controller
         $emergency = Emergency::query()->find($chatRoom->emergency_id);
         if(compareWithEnum($user->type, UserTypeEnum::USER)) {
             Log::info('USER ID ' . $user->id . ' reporting_user_id ' . $emergency->reporting_user_id);
-            if($user->user_id != $emergency->reporting_user_id) return false;
+            if($user->id != $emergency->reporting_user_id) return false;
         }
         else {
             $agent = $user->getAgent();
