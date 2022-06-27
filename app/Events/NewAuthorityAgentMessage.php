84d006c373c31e6caab1dc5646665d47c1ef8f2c
Kaan Serin <kaanserin@Kaans-MacBook-Pro.local>
2022-06-27 05:04:49 +0300

    Update

--- app/Events/NewAuthorityAgentMessage.php
+++ app/Events/NewAuthorityAgentMessage.php
@@ -7,11 +7,12 @@ use App\Models\ChatMessage;
 use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class NewAuthorityAgentMessage
+class NewAuthorityAgentMessage implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
